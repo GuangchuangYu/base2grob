ade643dab8fc18180adfe20d7ebe24b22361b8dc
GuangchuangYu <guangchuangyu@gmail.com>
2018-03-15 00:31:10 +0800

    set base par

--- R/toGrob.R
+++ R/toGrob.R
@@ -5,7 +5,7 @@
 ##' @param x expression or formula of base plot function call. e.g. expression(pie(1:5)) or ~plot(1:10, 1:10)
 ##' @return grob object
 ##' @importFrom gridGraphics grid.echo
-##' @importFrom grid grid.grab
+##' @importFrom grid grid.grabExpr
 ##' @importFrom grid grid.draw
 ##' @export
 ##' @examples
@@ -15,9 +15,13 @@ base2grob <- function(x) {
     if (!inherits(x, "expression") && !inherits(x, "formula")) {
         stop("input should be expression or formula of base plot function call")
     }
-    tmp <- grid.draw(x) ## base plot may return value via `invisible()`
-    grid.echo()
-    invisible(grid.grab())
+    old.par=par(no.readonly=TRUE)
+    on.exit(suppressWarnings(par(old.par, no.readonly=TRUE)))
+
+    plot_fun <- function() grid.draw(x)
+    grob <- grid.grabExpr(grid.echo(plot_fun))
+    
+    invisible(grob)
 }
 
 
@@ -25,6 +29,14 @@ base2grob <- function(x) {
 ##' @method grid.draw expression
 ##' @export
 grid.draw.expression <- function(x, recording = TRUE) {
+
+    ## taken from cowplot vignette
+    par(xpd = NA, # switch off clipping, necessary to always see axis labels
+        bg = "transparent", # switch off background to avoid obscuring adjacent plots
+        oma = c(2, 2, 0, 0), # move plot to the right and up
+        mgp = c(2, 1, 0) # move axis labels closer to axis
+        )
+
     eval(x)
 }
 
@@ -32,6 +44,6 @@ grid.draw.expression <- function(x, recording = TRUE) {
 ##' @export
 grid.draw.formula <- function(x, recording = TRUE) {
     xx = as.character(x)[2]
-    eval(parse(text=xx))
+    grid.draw.expression(parse(text=xx))
 }
 
